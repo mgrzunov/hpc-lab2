5d2e40c3335fe2ff479f7dc5a57648bd46514e2c
Matej Grz <matej.grzunov@fer.hr>
2023-05-17 23:09:46 +0200

    Loading entire video before processing

--- sequential.c
+++ sequential.c
@@ -4,18 +4,28 @@
 #include <string.h>
 #include <time.h>
 
+static clock_t total_time;
 static clock_t start_time;
 static clock_t stop_time;
 
 #define MEASURE_START() (start_time = clock())
-#define MEASURE_STOP()  (stop_time = clock())
+#define MEASURE_STOP()  \
+    do { \
+        stop_time = clock(); \
+        total_time += stop_time - start_time; \
+    } while (0)
 #define MEASURE_PRINT(tag) (printf("[" tag "] Time = %.4f seconds\n", ((float)(stop_time - start_time)) / CLOCKS_PER_SEC))
+#define MEASURE_PRINT_TOTAL() (printf("[TOTAL] Time = %.4f seconds\n", (float)total_time / CLOCKS_PER_SEC))
 
 #define PIXEL_WIDTH 3840
 #define PIXEL_HEIGHT 2160
 #define PIXEL_NUM (PIXEL_HEIGHT * PIXEL_WIDTH)
 #define FRAME_NUM 60
-#define VIDEO_SIZE (PIXEL_NUM * 3 * 60)
+#define COMPONENT_NUM 3
+#define FRAME_SIZE (COMPONENT_NUM * PIXEL_NUM)
+#define VIDEO_SIZE (FRAME_SIZE * FRAME_NUM)
+
+#define UNDERSAMPLED_FRAME_SIZE (PIXEL_NUM + PIXEL_NUM / 2)
 
 #define FILE_NAME_WIDTH 512
 
@@ -62,143 +72,179 @@ int main(int argc, char *argv[])
     }
 
     // Program uses only 2 frame buffers
-    uint8_t *frame_data_1 = malloc(sizeof(*frame_data_1) * PIXEL_NUM * 3);
-    uint8_t *frame_data_2 = malloc(sizeof(*frame_data_1) * PIXEL_NUM * 3);
+    uint8_t *video_data_1 = malloc(sizeof(*video_data_1) * VIDEO_SIZE);
+    uint8_t *video_data_2 = malloc(sizeof(*video_data_1) * VIDEO_SIZE);
 
-    // Start measuring
+    size_t byte_count;
+    uint8_t *rgb_video_data = &video_data_1[0];
+    uint8_t *yuv_video_data = &video_data_1[0]; // Can use same buffer
+    uint8_t *yuv_undersampled_data = &video_data_2[0]; // Switches buffer
+    uint8_t *yuv_oversampled_data = &video_data_1[0]; // Switches buffer
+
+    /**********************/
+    /*** LOAD RGB VIDEO ***/
+    /**********************/
     MEASURE_START();
+    byte_count = fread(
+        &rgb_video_data[0],
+        sizeof(video_data_1[0]), VIDEO_SIZE, 
+        rgb_video_file
+    );
+    if (byte_count != VIDEO_SIZE)
+    {
+        fprintf(stderr, "Only loaded %u bytes of original!\n", (uint)byte_count);
+        return -1;
+    }
+    MEASURE_STOP();
+    MEASURE_PRINT("Load RGB");
 
-    // Process all 60 frames
+    /*******************/
+    /*** RGB --> YUV ***/
+    /*******************/
+    MEASURE_START();
     for (uint frame_i = 0; frame_i != FRAME_NUM; ++frame_i)
     {
-        size_t byte_count;
-        uint8_t *rgb_frame_data = &frame_data_1[0];
-
-        /**********************/
-        /*** LOAD RGB FRAME ***/
-        /**********************/
-        byte_count = fread(
-            &rgb_frame_data[0],
-            sizeof(frame_data_1[0]), PIXEL_NUM * 3, 
-            rgb_video_file
-        );
-        if (byte_count != PIXEL_NUM * 3)
-        {
-            fprintf(stderr, "[f%d - 1] Only loaded %lu bytes of original!\n", frame_i, byte_count);
-            break;
-        }
-
-        /*******************/
-        /*** RGB --> YUV ***/
-        /*******************/
-        uint8_t *yuv_frame_data = &frame_data_1[0]; // Can use same buffer
         for (uint pixel_i = 0; pixel_i != PIXEL_NUM; ++pixel_i)
         {
-            const uint8_t r_value = rgb_frame_data[pixel_i];
-            const uint8_t g_value = rgb_frame_data[pixel_i + PIXEL_NUM];
-            const uint8_t b_value = rgb_frame_data[pixel_i + PIXEL_NUM * 2];
+            const uint8_t r_value = rgb_video_data[frame_i * FRAME_SIZE + pixel_i];
+            const uint8_t g_value = rgb_video_data[frame_i * FRAME_SIZE + pixel_i + PIXEL_NUM];
+            const uint8_t b_value = rgb_video_data[frame_i * FRAME_SIZE + pixel_i + PIXEL_NUM * 2];
 
             const uint8_t y_value = (uint8_t)(0.257f * r_value + 0.504f * g_value + 0.098f * b_value + 16);
             const uint8_t u_value = (uint8_t)(-0.148f * r_value - 0.291f * g_value + 0.439f * b_value + 128);
             const uint8_t v_value = (uint8_t)(0.439f * r_value - 0.368f * g_value - 0.071 * b_value + 128);
 
-            yuv_frame_data[pixel_i]                 = y_value;
-            yuv_frame_data[pixel_i + PIXEL_NUM]     = u_value;
-            yuv_frame_data[pixel_i + PIXEL_NUM * 2] = v_value;
+            yuv_video_data[frame_i * FRAME_SIZE + pixel_i]                 = y_value;
+            yuv_video_data[frame_i * FRAME_SIZE + pixel_i + PIXEL_NUM]     = u_value;
+            yuv_video_data[frame_i * FRAME_SIZE + pixel_i + PIXEL_NUM * 2] = v_value;
         }
-        byte_count = fwrite(
-            &yuv_frame_data[0],
-            sizeof(frame_data_1[0]), PIXEL_NUM * 3, 
-            yuv_file
-        );
-        if (byte_count != PIXEL_NUM * 3)
-        {
-            fprintf(stderr, "[f%d - 2] Only wrote %lu bytes of original!\n", frame_i, byte_count);
-            break;
-        }
+    }
+    MEASURE_STOP();
+    MEASURE_PRINT("RGB to YUV");
 
-        /*************************************/
-        /*** Undersampling 4:4:4 --> 4:2:0 ***/
-        /*************************************/
-        uint8_t *yuv_undersampled_data = &frame_data_2[0]; // Uses other buffer
-        memcpy(&yuv_undersampled_data[0], &yuv_frame_data[0], PIXEL_NUM); // Just copy Y values
+    MEASURE_START();
+    byte_count = fwrite(
+        &yuv_video_data[0],
+        sizeof(video_data_1[0]), VIDEO_SIZE, 
+        yuv_file
+    );
+    if (byte_count != VIDEO_SIZE)
+    {
+        fprintf(stderr, "Only wrote %u bytes of yuv video!\n", (uint)byte_count);
+        return -1;
+    }
+    MEASURE_STOP();
+    //MEASURE_PRINT("Store YUV"); Measuring for total time
+
+    /*************************************/
+    /*** Undersampling 4:4:4 --> 4:2:0 ***/
+    /*************************************/
+    MEASURE_START();
+    for (uint frame_i = 0; frame_i != FRAME_NUM; ++frame_i)
+    {
+        // Just copy Y values
+        memcpy(
+            &yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE], 
+            &yuv_video_data[frame_i * FRAME_SIZE], 
+            PIXEL_NUM
+        );
         for (uint row = 0; row != PIXEL_HEIGHT; row += 2)
         {            
             for (uint col = 0; col != PIXEL_WIDTH; col += 2)
             {
                 // Average 4 U values into 1 (+ PIXEL_NUM because Y is preserved)
-                yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM] = (
-                    yuv_frame_data[   row    * PIXEL_WIDTH + col     + PIXEL_NUM] +
-                    yuv_frame_data[   row    * PIXEL_WIDTH + col + 1 + PIXEL_NUM] +
-                    yuv_frame_data[(row + 1) * PIXEL_WIDTH + col     + PIXEL_NUM] +
-                    yuv_frame_data[(row + 1) * PIXEL_WIDTH + col + 1 + PIXEL_NUM]
+                yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM] = (
+                    yuv_video_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col     + PIXEL_NUM] +
+                    yuv_video_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col + 1 + PIXEL_NUM] +
+                    yuv_video_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col     + PIXEL_NUM] +
+                    yuv_video_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col + 1 + PIXEL_NUM]
                 ) / 4;
                 // Average 4 V values into 1 (+ PIXEL_NUM + PIXEL_NUM / 4 for Y and U offset)
-                yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)] = (
-                    yuv_frame_data[   row    * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] +
-                    yuv_frame_data[   row    * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)] +
-                    yuv_frame_data[(row + 1) * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] +
-                    yuv_frame_data[(row + 1) * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)]
+                yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)] = (
+                    yuv_video_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] +
+                    yuv_video_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)] +
+                    yuv_video_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] +
+                    yuv_video_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)]
                 ) / 4;
             }
         }
-        byte_count = fwrite(
-            &yuv_undersampled_data[0],
-            sizeof(frame_data_1[0]), PIXEL_NUM + PIXEL_NUM / 2, 
-            yuv_undersampled_file
-        );
-        if (byte_count != PIXEL_NUM + PIXEL_NUM / 2)
-        {
-            fprintf(stderr, "[f%d - 3] Only wrote %lu bytes of original!\n", frame_i, byte_count);
-            break;
-        }
+    }
+    MEASURE_STOP();
+    MEASURE_PRINT("Undersample YUV");
 
-        /*************************************/
-        /*** Oversampling  4:2:0 --> 4:4:4 ***/
-        /*************************************/
-        uint8_t *yuv_oversampled_data = &frame_data_1[0]; // Uses other buffer
-        memcpy(&yuv_oversampled_data[0], &yuv_undersampled_data[0], PIXEL_NUM); // Just copy Y values
+    MEASURE_START();
+    byte_count = fwrite(
+        &yuv_undersampled_data[0],
+        sizeof(video_data_1[0]), UNDERSAMPLED_FRAME_SIZE * FRAME_NUM, 
+        yuv_undersampled_file
+    );
+    if (byte_count != UNDERSAMPLED_FRAME_SIZE * FRAME_NUM)
+    {
+        fprintf(stderr, "Only wrote %u bytes of undersampled video!\n", (uint)byte_count);
+        return -1;
+    }
+    MEASURE_STOP();
+    //MEASURE_PRINT("Store undersampled"); Measuring for total time
+
+    /*************************************/
+    /*** Oversampling  4:2:0 --> 4:4:4 ***/
+    /*************************************/
+    MEASURE_START();
+    for (uint frame_i = 0; frame_i != FRAME_NUM; ++frame_i)
+    {
+        // Just copy Y values
+        memcpy(
+            &yuv_oversampled_data[frame_i * FRAME_SIZE], 
+            &yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE], 
+            PIXEL_NUM
+        ); 
         for (uint row = 0; row != PIXEL_HEIGHT; row += 2)
         {
             for (uint col = 0; col != PIXEL_WIDTH; col += 2)
             {
+                // Inverse operation of undersampling
                 // Oversample U component
-                yuv_oversampled_data[   row    * PIXEL_WIDTH + col     + PIXEL_NUM] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
-                yuv_oversampled_data[   row    * PIXEL_WIDTH + col + 1 + PIXEL_NUM] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
-                yuv_oversampled_data[(row + 1) * PIXEL_WIDTH + col     + PIXEL_NUM] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
-                yuv_oversampled_data[(row + 1) * PIXEL_WIDTH + col + 1 + PIXEL_NUM] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
+                yuv_oversampled_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col     + PIXEL_NUM] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
+                yuv_oversampled_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col + 1 + PIXEL_NUM] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
+                yuv_oversampled_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col     + PIXEL_NUM] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
+                yuv_oversampled_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col + 1 + PIXEL_NUM] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + PIXEL_NUM]; 
 
                 // Oversample V component
-                yuv_oversampled_data[   row    * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)]; 
-                yuv_oversampled_data[   row    * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)];
-                yuv_oversampled_data[(row + 1) * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)];
-                yuv_oversampled_data[(row + 1) * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)] =
-                    yuv_undersampled_data[(row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)];
+                yuv_oversampled_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)]; 
+                yuv_oversampled_data[frame_i * FRAME_SIZE +    row    * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)];
+                yuv_oversampled_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col     + (2 * PIXEL_NUM)] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)];
+                yuv_oversampled_data[frame_i * FRAME_SIZE + (row + 1) * PIXEL_WIDTH + col + 1 + (2 * PIXEL_NUM)] =
+                    yuv_undersampled_data[frame_i * UNDERSAMPLED_FRAME_SIZE + (row / 2) * (PIXEL_WIDTH / 2) + (col / 2) + (PIXEL_NUM + PIXEL_NUM / 4)];
             }
         }
-        byte_count = fwrite(
-            &yuv_oversampled_data[0],
-            sizeof(frame_data_1[0]), PIXEL_NUM * 3, 
-            yuv_oversampled_file
-        );
-        if (byte_count != PIXEL_NUM * 3)
-        {
-            fprintf(stderr, "[f%d - 4] Only wrote %lu bytes of original!\n", frame_i, byte_count);
-            break;
-        }
     }
-
-    // Stop measuring
     MEASURE_STOP();
-    // Print measurement
-    MEASURE_PRINT("Total");
+    MEASURE_PRINT("Oversample YUV");
+
+    MEASURE_START();
+    byte_count = fwrite(
+        &yuv_oversampled_data[0],
+        sizeof(video_data_1[0]), VIDEO_SIZE, 
+        yuv_oversampled_file
+    );
+    if (byte_count != VIDEO_SIZE)
+    {
+        fprintf(stderr, "Only wrote %u bytes of oversampled video!\n", (uint)byte_count);
+        return -1;
+    }
+    MEASURE_STOP();
+    //MEASURE_PRINT("Store oversampled"); Measuring for total time
+
+    // Print total time
+    MEASURE_PRINT_TOTAL();
+
     
     /*************************/
     /*** CLOSE VIDEO FILES ***/
